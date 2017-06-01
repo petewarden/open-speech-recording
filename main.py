9881c87c764267957f6eb617a79dcee09462c80d
Pete Warden <petewarden@google.com>
2017-06-01 16:05:37 -0700

    Initial import

---
+++ main.py
@@ -0,0 +1,78 @@
+from flask import Flask
+from flask import abort
+from flask import make_response
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import session
+from werkzeug.utils import secure_filename
+
+from google.cloud import storage
+
+import os
+import uuid
+
+app = Flask(__name__)
+
+# Configure this environment variable via app.yaml
+CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
+# [end config]
+
+@app.route("/")
+def welcome():
+    session_id = request.cookies.get('session_id')
+    if session_id:
+        all_done = request.cookies.get('all_done')
+        if all_done:
+            return render_template("thanks.html")
+        else:
+            return render_template("record.html")
+    else:
+        return render_template("welcome.html")
+
+@app.route("/start")
+def start():
+    response = make_response(redirect('/'))
+    session_id = uuid.uuid4().hex
+    response.set_cookie('session_id', session_id)
+    return response
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    session_id = request.cookies.get('session_id')
+    if not session_id:
+        make_response('No session', 400)
+    word = request.args.get('word')
+    audio_data = request.data
+    filename = word + '_' + session_id + '_' + uuid.uuid4().hex + '.ogg'
+    secure_name = secure_filename(filename)
+    # Left in for debugging purposes. If you comment this back in, the data
+    # will be saved to the local file system.
+    #with open(secure_name, 'wb') as f:
+    #    f.write(audio_data)
+    # Create a Cloud Storage client.
+    gcs = storage.Client()
+    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+    blob = bucket.blob(secure_name)
+    blob.upload_from_string(audio_data, content_type='audio/ogg')
+    return make_response('All good')
+
+# CSRF protection, see http://flask.pocoo.org/snippets/3/.
+@app.before_request
+def csrf_protect():
+    if request.method == "POST":
+        token = session['_csrf_token']
+        if not token or token != request.args.get('_csrf_token'):
+            abort(403)
+
+def generate_csrf_token():
+    if '_csrf_token' not in session:
+        session['_csrf_token'] = uuid.uuid4().hex
+    return session['_csrf_token']
+
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
+# Change this to your own number before you deploy.
+app.secret_key = os.environ['SESSION_SECRET_KEY']
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/scripts/app.js
@@ -0,0 +1,378 @@
+// fork getUserMedia for multiple browser versions, for the future
+// when more browsers support MediaRecorder
+
+navigator.getUserMedia = ( navigator.getUserMedia ||
+                       navigator.webkitGetUserMedia ||
+                       navigator.mozGetUserMedia ||
+                       navigator.msGetUserMedia);
+
+// set up basic variables for app
+
+var record = document.querySelector('.record');
+var stop = document.querySelector('.stop');
+var upload = document.querySelector('.upload');
+var soundClips = document.querySelector('.sound-clips');
+var canvas = document.querySelector('.visualizer');
+var mediaRecorder = null;
+
+// disable stop button while not recording
+
+stop.disabled = true;
+upload.disabled = true;
+
+// visualiser setup - create web audio api context and canvas
+
+var audioCtx = new (window.AudioContext || webkitAudioContext)();
+var canvasCtx = canvas.getContext("2d");
+
+//main block for doing the audio recording
+
+if (navigator.getUserMedia) {
+  console.log('getUserMedia supported.');
+
+  var constraints = { audio: true };
+  var chunks = [];
+
+  var onSuccess = function(stream) {
+    mediaRecorder = new MediaRecorder(stream);
+
+    visualize(stream);
+
+    record.onclick = function() {
+      // Display a countdown before recording starts.
+      var progress = document.querySelector('.progress-display');
+      progress.innerText = "3";
+      document.querySelector('.info-display').innerText = "";
+      setTimeout(function() {
+	  progress.innerText = "2";
+	  setTimeout(function() {
+	      progress.innerText = "1";
+	      setTimeout(function() {
+		  progress.innerText = "";
+		  startRecording();
+	      }, 1000);
+	  }, 1000);
+      }, 1000);
+      stop.disabled = false;
+      record.disabled = true;
+    }
+
+    stop.onclick = function() {
+      mediaRecorder.stop();
+      console.log(mediaRecorder.state);
+      console.log("recorder stopped");
+      record.style.background = "";
+      record.style.color = "";
+ 
+      stop.disabled = true;
+      record.disabled = false;
+    }
+
+    upload.onclick = function() {
+      saveRecordings();
+    }
+
+    mediaRecorder.onstop = function(e) {
+      console.log("data available after MediaRecorder.stop() called.");
+
+      var clipName = document.querySelector('.info-display').innerText;
+      var clipContainer = document.createElement('article');
+      var clipLabel = document.createElement('p');
+      var audio = document.createElement('audio');
+      var deleteButton = document.createElement('button');
+     
+      clipContainer.classList.add('clip');
+      clipLabel.classList.add('clip-label');
+      audio.setAttribute('controls', '');
+      deleteButton.textContent = 'Delete';
+      deleteButton.className = 'delete';
+      clipLabel.textContent = clipName;
+
+      clipContainer.appendChild(audio);
+      clipContainer.appendChild(clipLabel);
+      clipContainer.appendChild(deleteButton);
+      soundClips.appendChild(clipContainer);
+
+      audio.controls = true;
+      var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+      chunks = [];
+      var audioURL = window.URL.createObjectURL(blob);
+      audio.src = audioURL;
+      console.log("recorder stopped");
+
+      deleteButton.onclick = function(e) {
+        evtTgt = e.target;
+        evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+	updateProgress();
+      }
+    }
+
+    mediaRecorder.ondataavailable = function(e) {
+      chunks.push(e.data);
+    }
+  }
+
+  var onError = function(err) {
+    console.log('The following error occured: ' + err);
+  }
+
+  navigator.getUserMedia(constraints, onSuccess, onError);
+} else {
+  console.log('getUserMedia not supported on your browser!');
+  document.querySelector('.info-display').innerText = 
+	'Your device does not support the HTML5 API needed to record audio (this is a known problem on iOS)';  
+}
+
+function visualize(stream) {
+  var source = audioCtx.createMediaStreamSource(stream);
+
+  var analyser = audioCtx.createAnalyser();
+  analyser.fftSize = 2048;
+  var bufferLength = analyser.frequencyBinCount;
+  var dataArray = new Uint8Array(bufferLength);
+
+  source.connect(analyser);
+  
+  WIDTH = canvas.width
+  HEIGHT = canvas.height;
+
+  draw()
+
+  function draw() {
+
+    requestAnimationFrame(draw);
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+    canvasCtx.beginPath();
+
+    var sliceWidth = WIDTH * 1.0 / bufferLength;
+    var x = 0;
+
+    for(var i = 0; i < bufferLength; i++) {
+ 
+      var v = dataArray[i] / 128.0;
+      var y = v * HEIGHT/2;
+
+      if(i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+
+      x += sliceWidth;
+    }
+
+    canvasCtx.lineTo(canvas.width, canvas.height/2);
+    canvasCtx.stroke();
+  }
+}
+
+var wantedWords = [
+  // 'Zero',
+  // 'One',
+  // 'Two',
+  // 'Three',
+  // 'Four',
+  // 'Five',
+  // 'Six',
+  // 'Seven',
+  // 'Eight',
+  // 'Nine',
+  // 'On',
+  // 'Off',
+  // 'Stop',
+  // 'Go',
+  // 'Up',
+  // 'Down',
+  // 'Left',
+  // 'Right',
+  // 'Yes',
+  // 'No',
+];
+
+var fillerWords = [
+  'Dog',
+  'Cat',
+  'Bird',
+  'Tree',
+  'Marvin',
+  'Sheila',
+  'House',
+  'Bed',
+  'Wow',
+  'Happy',
+];
+
+function getRecordedWords() {
+  var wordElements = document.querySelectorAll('.clip-label');
+  var wordCounts = {};
+  wordElements.forEach(function(wordElement) {
+      var word = wordElement.innerText;
+      if (!wordCounts.hasOwnProperty(word)) {
+	  wordCounts[word] = 0;
+      }
+      wordCounts[word] += 1;
+  });
+  return wordCounts;
+}
+
+function getAllWantedWords() {
+  var wordCounts = {};
+  wantedWords.forEach(function(word) {
+    wordCounts[word] = 5;
+  });
+  fillerWords.forEach(function(word) {
+    wordCounts[word] = 1;
+  });
+  return wordCounts;
+}
+
+function getRemainingWords() {
+  var recordedCounts = getRecordedWords();
+  var wantedCounts = getAllWantedWords();
+  var remainingCounts = {};
+  for (var word in wantedCounts) {
+    wantedCount = wantedCounts[word];
+    var recordedCount;
+    if (recordedCounts.hasOwnProperty(word)) {
+      recordedCount = recordedCounts[word];
+    } else {
+      recordedCount = 0;
+    }
+    var remainingCount = wantedCount - recordedCount;
+    if (remainingCount > 0) {
+      remainingCounts[word] = remainingCount;
+    }
+  }
+  return remainingCounts;
+}
+
+function unrollWordCounts(wordCounts) {
+  var result = [];
+  for (var word in wordCounts) {
+    count = wordCounts[word];
+    for (var i = 0; i < count; ++i) {
+      result.push(word);
+    }
+  }
+  return result;
+}
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+function getNextWord() {
+  var remainingWords = unrollWordCounts(getRemainingWords());
+  if (remainingWords.length == 0) {
+    return null;
+  }
+  shuffleArray(remainingWords);
+  return remainingWords[0];
+}
+
+function getProgressDescription() {
+  var allWords = unrollWordCounts(getAllWantedWords());
+  var remainingWords = unrollWordCounts(getRemainingWords());
+  return ((allWords.length + 1) - remainingWords.length) + "/" + allWords.length;
+}
+
+function updateProgress() {
+  var progress = getProgressDescription();
+  document.querySelector('.progress-display').innerText = progress;
+}
+
+function startRecording() {
+  var word = getNextWord();
+  if (word === null) {
+    promptToSave();
+    return;
+  }
+  updateProgress();
+  document.querySelector('.info-display').innerText = word;
+  mediaRecorder.start();
+  console.log(mediaRecorder.state);
+  console.log("recorder started");
+  record.style.background = "red";
+  setTimeout(endRecording, 1500);
+}
+
+function endRecording() {
+  mediaRecorder.stop();
+  console.log(mediaRecorder.state);
+  console.log("recorder stopped");
+  record.style.background = "";
+  record.style.color = "";
+  setTimeout(startRecording, 1000);
+}
+
+function promptToSave() {
+  if (confirm('Are you ready to upload your words?\nIf not, press cancel now,' + 
+	      ' and then press Upload once you are ready.')) {
+    saveRecordings();
+  }
+  upload.disabled = false;
+}
+
+var allClips;
+var clipIndex;
+
+function saveRecordings() {
+  allClips = document.querySelectorAll('.clip');
+  clipIndex = 0;
+  uploadNextClip();
+}
+
+function uploadNextClip() {
+  document.querySelector('.progress-display').innerText = 'Uploading clip ' + 
+	clipIndex + '/' + unrollWordCounts(getAllWantedWords()).length;
+  var clip = allClips[clipIndex];
+  var audioBlobUrl = clip.querySelector('audio').src;
+  var word = clip.querySelector('p').innerText;
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', audioBlobUrl, true);
+  xhr.responseType = 'blob';
+  xhr.onload = function(e) {
+    if (this.status == 200) {
+      var blob = this.response;
+      var ajaxRequest = new XMLHttpRequest();
+      var uploadUrl = '/upload?word=' + word + '&_csrf_token=' + csrf_token;
+      ajaxRequest.open('POST', uploadUrl, true);
+      ajaxRequest.setRequestHeader('Content-Type', 'application/json');    
+      ajaxRequest.setRequestHeader('Content-Type', 'application/json');    
+      ajaxRequest.onreadystatechange = function() {
+        if (ajaxRequest.readyState == 4) {
+	  if (ajaxRequest.status === 200) {
+            clipIndex += 1;
+            if (clipIndex < allClips.length) {
+	      uploadNextClip();
+	    } else {
+	      allDone();
+	    }
+          } else {
+            alert('Uploading failed with error code ' + ajaxRequest.status);
+          }
+	}
+      };
+      ajaxRequest.send(blob);
+    }
+  };
+  xhr.send();
+}
+
+function allDone() {
+  document.cookie = 'all_done=true; path=/';
+  location.reload(true);
+}
